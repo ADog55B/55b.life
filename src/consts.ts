909c265bf375d13ca112ae88048b55b54517f8ad
PiPiCat <adog55b@gmail.com>
2026-04-18 02:11:34 -0700

    feat:主页修改

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,60 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "55b.life",
+  EMAIL: "pipineko@outlook.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 1,
+  NUM_PROJECTS_ON_HOMEPAGE: 1,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  TITLE: "主页",
+  DESCRIPTION: "About me",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  TITLE: "文章",
+  DESCRIPTION: "脑袋里装的长篇大论。",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  TITLE: "正发生",
+  DESCRIPTION: "此刻在想什么？",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  TITLE: "好玩",
+  DESCRIPTION: "随便你吧。",
 };
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    NAME: "微博",
+    HREF: "https://www.weibo.com/u/7754908874",
   },
   { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    NAME: "Threads（同步微博）",
+    HREF: "https://www.threads.com/@55bisacat"
   },
   { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    NAME: "Instagram",
+    HREF: "https://www.instagram.com/55bisacat/"
+  },
+  { 
+    NAME: "豆瓣",
+    HREF: "https://www.douban.com/people/128785937/",
+  },  
+  { 
+    NAME: "小红书",
+    HREF: "https://xhslink.com/m/52zmvxUd7Nw",
+  },  
+  { 
+    NAME: "播客《非必要不》",
+    HREF: "https://www.xiaoyuzhoufm.com/podcast/643446bea79cc8147028b723",
+  },  
+  { 
+    NAME: "公众号 妙控猫咪（请手动搜索）",
+    HREF: " ",
   }
 ];
--- src/content/work/apple.md
+++ src/content/work/apple.md
@@ -1,8 +1,8 @@
 ---
-company: "Apple"
-role: "Software Engineer"
-dateStart: "01/01/2020"
-dateEnd: "11/27/2022"
+company: "Foster City"
+role: "休息中"
+dateStart: "12/15/2024"
+dateEnd: "Right now"
 ---
 
-Voluptatem est quaerat voluptas praesentium ipsa dolorem dignissimos nulla ratione distinctio quae maiores eligendi nostrum? Quibusdam, debitis voluptatum, lorem ipsum dolor. Sit amet consectetur adipisicing elit. Iure illo neque tempora.
+我在休息哦。
